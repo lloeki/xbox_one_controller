3bdc0f68ff079ec35e0685b3a0e9f3e18efb89d7
Loic Nageleisen <loic.nageleisen@gmail.com>
2015-02-06 13:19:19 +0100

    auto-find kext artifact

--- reload.sh
+++ reload.sh
@@ -2,5 +2,5 @@
 
 sudo kextunload /tmp/xbox_one_controller.kext;
 sleep 1;
-sudo cp -R /Users/lloeki/Library/Developer/Xcode/DerivedData/xbox_one_controller-cbgpvvfvgbipuyhbfzvnjwhtnqfh/Build/Products/Debug/xbox_one_controller.kext /tmp/ &&
+sudo cp -R /Users/lloeki/Library/Developer/Xcode/DerivedData/xbox_one_controller-*/Build/Products/Debug/xbox_one_controller.kext /tmp/ &&
 sudo kextload /tmp/xbox_one_controller.kext
