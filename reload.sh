444e4b03c59135904512e937a47d438af760af71
Loic Nageleisen <loic.nageleisen@gmail.com>
2015-02-06 16:29:43 +0100

    sync before reload

--- reload.sh
+++ reload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sync
 sudo kextunload /tmp/xbox_one_controller.kext;
 sleep 1;
 sudo cp -R /Users/lloeki/Library/Developer/Xcode/DerivedData/xbox_one_controller-*/Build/Products/Debug/xbox_one_controller.kext /tmp/ &&
