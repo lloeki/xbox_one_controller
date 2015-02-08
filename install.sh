36cc5f00221e64d6e03116ad483a7b9c21f3a16a
Loic Nageleisen <loic.nageleisen@gmail.com>
2015-02-08 16:47:55 +0100

    package and install script

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+set -o nounset
+
+function finish () {
+    if [[ $? -eq 0 ]]; then
+        echo "Installation complete! Reboot to finish."
+    else
+        echo "Installation failed!"
+    fi
+    read -p "-- Press a key --" -n 1 -r
+}
+trap finish EXIT
+
+# check source
+kext_name='xbox_one_controller.kext'
+kext_to_install="$(cd "$(dirname "${BASH_SOURCE[0]}")" && echo "$PWD")/$kext_name"
+if [[ -d "$kext_to_install" ]]; then
+    echo "I will install '$kext_to_install' into /Library/Extensions"
+else
+    echo "Error: kext not found '$kext_to_install'"
+    exit 1
+fi
+
+# check dev mode
+if nvram -p | grep 'boot-args' | grep -q 'kext-dev-mode=1'; then
+    echo "kext developer mode detected"
+else
+    echo "Warning: kext developer mode NOT detected"
+    read -p "Add it to kernel boot flags? [y/N] " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "OK, exiting..."
+        exit 1
+    else
+        sudo nvram boot-args=kext-dev-mode=1
+    fi
+fi
+
+# unload driver
+if kextstat | grep -q com.lloeki.xbox-one-controller; then
+    if sudo kextunload /Library/Extensions/xbox_one_controller.kext; then
+        echo "Previous kext unloaded"
+    else
+        echo "Could not unload driver, please unplug any controller and rerun"
+        exit 1
+    fi
+fi
+
+# uninstall
+echo "Removing previous version..."
+sync
+sudo rm -rf /Library/Extensions/xbox_one_controller.kext
+
+# install
+echo "Installing new version..."
+sync
+sudo cp -R xbox_one_controller.kext /tmp
+sudo mv /tmp/xbox_one_controller.kext /Library/Extensions
---
+++ package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+out="xbox_one_controller"
+tmp_dir="$(mktemp -d -t xbox_one_controller)"
+echo "building in '$tmp_dir'..."
+
+mkdir "$tmp_dir/xbox_one_controller"
+sudo cp -R /Users/lloeki/Library/Developer/Xcode/DerivedData/xbox_one_controller-*/Build/Products/Debug/xbox_one_controller.kext "$tmp_dir/xbox_one_controller"
+sudo cp install.sh "$tmp_dir/xbox_one_controller"
+
+cd "$tmp_dir"
+zip -r "$out.zip" xbox_one_controller
+cd -
+mv "$tmp_dir/$out.zip" .
